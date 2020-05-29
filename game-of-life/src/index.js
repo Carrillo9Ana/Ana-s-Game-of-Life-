d66a1804bee4dadbb4c9a98304581b4bab27be42
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-05-28 22:00:23 -0400

    close to full functionality, buttons are implemented as well as style

--- game-of-life/src/index.js
+++ game-of-life/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { ButtonToolbar, DropdownItem, DropdownButton } from 'react-bootstrap';
 
 class Box extends React.Component {
   
@@ -18,7 +19,7 @@ class Box extends React.Component {
 class Grid extends React.Component {
   
   render() {
-    const width = (this.props.cols * 16) + 1;
+    const width = (this.props.cols * 14) + 1;
     var rowsArr = [];
     var boxClass = "";
     for (var i = 0; i < this.props.rows; i++) {
@@ -47,6 +48,50 @@ class Grid extends React.Component {
   }
 }
 
+class Buttons extends React.Component {
+
+	handleSelect = (evt) => {
+		this.props.gridSize(evt);
+	}
+
+	render() {
+		return (
+			<div className="center">
+				<ButtonToolbar>
+					<button class="button" variant="light" onClick={this.props.playButton}>
+						Play
+					</button>
+					<button class="button" variant="light" onClick={this.props.pauseButton}>
+					  Pause
+					</button>
+					<button class="button" variant="light" onClick={this.props.clear}>
+					  Clear
+					</button>
+					<button class="button" variant="light" onClick={this.props.slow}>
+					  Slow
+					</button>
+					<button class="button" variant="light" onClick={this.props.fast}>
+					  Fast
+					</button>
+					<button class="button" variant="light" onClick={this.props.seed}>
+					  Random
+					</button>
+					<DropdownButton
+            class="button"
+						title="Grid Size"
+						id="size-menu"
+            onSelect={this.handleSelect}
+					>
+						<DropdownItem eventKey="1">20x10</DropdownItem>
+						<DropdownItem eventKey="2">50x30</DropdownItem>
+						<DropdownItem eventKey="3">70x50</DropdownItem>
+					</DropdownButton>
+				</ButtonToolbar>
+			</div>
+			)
+	}
+}
+
 class Container extends React.Component {
     constructor() {
       super();
@@ -67,17 +112,115 @@ class Container extends React.Component {
       })
     }
 
+    seed = () => {
+      let gridCopy = arrayClone(this.state.gridFull);
+      for (let i = 0; i < this.rows; i ++) {
+        for (let j = 0; j < this.cols; j ++) {
+          if (Math.floor(Math.random() * 4) === 1) {
+            gridCopy[i][j] = true;
+          }
+        }
+      }
+      this.setState({
+        gridFull: gridCopy
+      });
+    }
+
+    playButton = () => {
+      clearInterval(this.intervalId);
+      this.intervalId = setInterval(this.play, this.speed);
+    }
+
+    pauseButton = () => {
+      clearInterval(this.intervalId);
+    }
+
+    slow = () => {
+      this.speed = 1000;
+      this.playButton();
+    }
+  
+    fast = () => {
+      this.speed = 100;
+      this.playButton();
+    }
+  
+    clear = () => {
+      var grid = Array(this.rows).fill().map(() => Array(this.cols).fill(false));
+      this.setState({
+        gridFull: grid,
+        generation: 0
+      });
+    }
+
+    gridSize = (size) => {
+      switch (size) {
+        case "1":
+          this.cols = 20;
+          this.rows = 10;
+          
+        break
+        case "2":
+          this.cols = 50;
+          this.rows = 30;
+        break
+        default:
+          this.cols = 70;
+          this.rows = 50;
+      }
+      this.clear();      
+    }
+
+    play = () => {
+      let g = this.state.gridFull;
+      let g2 = arrayClone(this.state.gridFull);
+
+      for (let i = 0; i < this.rows; i++) {
+        for (let j = 0; j < this.cols; j++) {
+          let count = 0;
+          if (i > 0) if (g[i - 1][j]) count++;
+          if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+          if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+          if (j < this.cols - 1) if (g[i][j + 1]) count++;
+          if (j > 0) if (g[i][j - 1]) count++;
+          if (i < this.rows - 1) if (g[i + 1][j]) count++;
+          if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+          if (i < this.rows - 1 && j < this.cols - 1) if (g[i + 1][j + 1]) count++;
+          if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+          if (!g[i][j] && count === 3) g2[i][j] = true;        
+        }
+      }
+      
+      this.setState({
+        gridFull: g2,
+        generation: this.state.generation + 1
+      });
+    }
+
+    componentDidMount() {
+      this.seed();
+    }
+
     render() {
         return (
             <div>
                 <h1>Conway's Game of Life</h1>
+                <Buttons
+                  playButton={this.playButton}
+                  pauseButton={this.pauseButton}
+                  slow={this.slow}
+                  fast={this.fast}
+                  clear={this.clear}
+                  seed={this.seed}
+                  gridSize={this.gridSize}
+                />
                 <Grid
                   gridFull={this.state.gridFull}
                   rows={this.rows}
                   cols={this.cols}
                   selectBox={this.selectBox}
                 />
-                <h2>Generations: {this.state.generation}</h2>    
+                <h1>Generations: {this.state.generation}</h1>    
             </div>
         );
     }
